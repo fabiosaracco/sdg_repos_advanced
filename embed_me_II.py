63087ad476086714bd80c6e1dffded6010f82c8b
sarawalk <f.sarawalk@gmail.com>
2025-04-01 10:06:27 +0200

    new tests

--- embed_me_II.py
+++ embed_me_II.py
@@ -72,11 +72,11 @@ def main():
 
             
             
-def file2ysn(file):
+def file2ysn(file, extension):
     '''
     From the file name return, respectively, the year, the sector and the name 
     '''
-    _split=file.replace('.rtf', '').split('_')
+    _split=file.replace(extension, '').split('_')
     #if ' ' in _split[-1] and len(_split[-1])>4:
     #    return _split[-1].split(' ')[0], _split[0].zfill(2), _split[-2]
     if '(' in _split[-1] or len(_split[-1])<4:
@@ -91,8 +91,8 @@ def vec_file_name(year, sector, name):
     myname=say_my_name(name)
     return year+'_'+sector+'_'+myname+'.txt'
 
-def file2vecfile(file):
-    y, s, n=file2ysn(file)
+def file2vecfile(file, extension='.rtf'):
+    y, s, n=file2ysn(file, extension)
     return vec_file_name(y, s, n)
 
 
